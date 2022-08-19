9e87d9efc0186b490f80ab6964fe614be8872d22
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-08-19 15:46:44 -0300

    agora vai

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 
-// finaliza tudo!!!
-
 require('dotenv/config');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
